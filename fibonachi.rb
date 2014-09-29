520c756a1724a93226fe7199dafaaa0fd25c81bc
Hunter Chapman <bootcoder@gmail.com>
2014-09-28 22:33:05 -0700

    CLI Stable

--- fibonachi.rb
+++ fibonachi.rb
@@ -6,6 +6,7 @@
 ##########################
 
 require 'awesome_print'
+require 'benchmark'
 
 ##########################
 # 			PSEUDO CODE
@@ -34,31 +35,41 @@ fib_container print each value
 =end
 
 
+##########################
+# 				Model 
+##########################
+
+
 def iterative_fibinacci(num)
 	fib_container = [1,1]
 	(num-2).times { fib_container << fib_container[-2] + fib_container.last }
-	fib_container.last
+	fib_container
 end
+
 
 def recursive_fibonacci(num, fib_container = [1,1])
 	fib_container << fib_container[-2] + fib_container.last
 	num -= 1
 	recursive_fibonacci(num, fib_container) unless num == 2
-	fib_container.last
+	fib_container
 end
+
+
 
 # write a function which takes a number and generates 
 # Fibonacci's until it surpasses it then returns true if the number is in sequence.
 
 def is_fibonacci(num)
 	fib_container = [1,1]
+	fib_counter = 2
 	until fib_container.last >= num
-		fib_container << fib_container[-2] + fib_container.last 
+		fib_container << fib_container[-2] + fib_container.last
+		fib_counter += 1
 	end
-	result = [num, (num == fib_container.last)]
+	result = [num, fib_container.last, (num == fib_container.last), fib_counter]
 end
-# puts "The number: #{num}, is a Fibonacci Sequenced Number." if fib_container.last == num
-# 	puts "The number: #{num}, is not a Fibonacci Sequenced Number." if fib_container.last != num
+
+
 
 
 ##########################
@@ -66,27 +77,155 @@ end
 ##########################
 
 def runFIBapp
-	greeting
+	system 'clear'
 	list_commands
-
+	list_command_controller
 end
 
-def greeting
-	puts "--Fibonacci Command Console--"
-end
 
-def list_commands
-	user_input
+def list_command_controller
+	input = user_input
+	system 'clear'
+	case input
+	when 1
+		choose_fib
+	when 2
+		check_fib
+	end
 end
 
 def user_input
 	print "->"
 	input = gets.chomp
-	ap input
+	terminate_program if input.downcase == "exit"
+	if input.to_i == 0
+		puts "Error:\tThis program accepts only numric input.\nPlease enter command number."
+		user_input
+	elsif input.to_i == 3
+		terminate_program
+	else 
+		return input.to_i
+	end
+end
+
+def choose_fib
+	ask_sequence
+	sequence = user_input
+	ask_function
+	func_type = user_input
+	case func_type
+	when 1
+		result = iterative_fibinacci(sequence)
+	when 2
+		result = recursive_fibonacci(sequence)
+	end
+	print_sequence_result(result, sequence)
+	prompt_continue
+	runFIBapp
+end
+
+def check_fib
+	ask_check_num
+	check_num = user_input
+	result = is_fibonacci(check_num)
+	print_check_result(result)
+	prompt_continue
+	runFIBapp
 end
 
 
+
+
+
+##########################
+# 				View 
+##########################
+
+def greeting
+	system 'clear'
+	puts
+	puts "\t--Fibonacci Calculation Console--\n\t\tHunter T. Chapman\n\t\s\s\s\s\s\s\sBootCoder@gmail.com"
+	puts 
+end
+
+def list_commands
+	greeting
+	puts "1. Find the number at a given position in Fibonacci sequence."
+	puts "2. Check if a number is within the Fibonacci sequence"
+	puts
+	puts "3. Terminate Program :: Enter 'Exit'"
+	puts
+	puts "Input Command Number:"
+end
+
+def ask_sequence
+	greeting
+	puts "What position in the sequence please:"
+end
+
+def ask_function
+	greeting
+	puts "This program can solve the sequence using iterative or recursive functionality."
+	puts
+	puts "Choose Function Type:"
+	puts "1. Iterative"
+	puts "2. Recursive"
+end
+
+def ask_check_num
+	greeting
+	puts "Please enter a positive integer to check \nif it is within the Fibonacci sequence."
+end
+
+def print_sequence_result(result, sequence)
+	greeting
+	puts "You requested the number at position #{sequence} in the Fibonacci sequence."
+	puts "The sequence is:"
+	print result
+	puts
+	puts
+	puts "Sequence position #{sequence}: = #{result.last}"
+end
+
+def print_check_result(result_arr)
+	greeting
+	puts "Checking Fibonacci Sequence for #{result_arr[0]}."
+	puts
+	puts
+	if result_arr[2] == true
+		puts "Success! The integer #{result_arr[1]} is a Fibonacci."
+		puts
+		puts "It is at position #{result_arr[3]} in the sequence."
+	else
+		puts "Sorry, #{result_arr[0]} is not in the sequence."
+		puts "The closest integer is #{result_arr[1]}."
+		puts "At position #{result_arr[3]}."
+		bench()
+	end
+end
+
+
+def prompt_continue
+	puts
+	puts "Hit Enter to Continue"
+	gets.chomp
+end
+
+def terminate_program
+	puts "Bye Bye Miss American Pie"
+	exit
+end
+
+
+
+
+##########################
+# 			APPLICATION
+##########################
 runFIBapp
+
+
+
 
 
 
@@ -94,9 +233,9 @@ runFIBapp
 # 			TEST CODE
 ##########################
 
-ap recursive_fibonacci(300) == 222232244629420445529739893461909967206666939096499764990979600
+# ap recursive_fibonacci(300).last == 222232244629420445529739893461909967206666939096499764990979600
 
-ap iterative_fibinacci(300) == 222232244629420445529739893461909967206666939096499764990979600
+# ap iterative_fibinacci(300).last == 222232244629420445529739893461909967206666939096499764990979600
 
-ap is_fibonacci(143)[1] == false
-ap is_fibonacci(144)[1] == true
+# ap is_fibonacci(143)[2] == false
+# ap is_fibonacci(144)[2] == true
